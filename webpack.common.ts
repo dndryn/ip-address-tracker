8a83185dd1c7e9dc813b5718b91eb5330ee33066
dndryn <dndryn@outlook.it>
2023-04-29 21:39:08 +0200

    refactor: Add local font loading

--- webpack.common.ts
+++ webpack.common.ts
@@ -5,39 +5,42 @@ import 'webpack-dev-server';
 
 
 const config: webpack.Configuration = {
-    target: 'web',
-    entry: path.resolve(__dirname, 'src', 'index.tsx'),
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'app.bundle.js',
+  target: "web",
+  entry: path.resolve(__dirname, "src", "index.tsx"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "app.bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.([cm]?ts|tsx)$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+    extensionAlias: {
+      ".js": [".js", ".ts"],
+      ".cjs": [".cjs", ".cts"],
+      ".mjs": [".mjs", ".mts"],
     },
-    module: {
-        rules: [
-            {
-                test: /\.([cm]?ts|tsx)$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.html$/,
-                use: ["html-loader"]
-            },
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-        ]
-    },
-    resolve:
-    {
-        extensions: ['.tsx', '.ts', '.js'],
-        extensionAlias: {
-            ".js": [".js", ".ts"],
-            ".cjs": [".cjs", ".cts"],
-            ".mjs": [".mjs", ".mts"]
-        },
-        plugins: [new TsconfigPathsPlugin({ configFile: "./tsconfig.json" })]
-    }
+    plugins: [new TsconfigPathsPlugin({ configFile: "./tsconfig.json" })],
+  },
 };
 
 export default config;
