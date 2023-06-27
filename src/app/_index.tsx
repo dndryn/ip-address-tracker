86f83dc6a36bbd8d8dcc8a7d57c1962528b0a607
dndryn <44530355+dndryn@users.noreply.github.com>
2023-06-27 17:21:18 +0200

    refactor: update of scss file import paths

--- src/app/_index.tsx
+++ src/app/_index.tsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Home from '@page/home';
 import '@css-reset'; // TODO: move to better location
 import '@font/Rubik'; // path: src\app\assets\css\Rubik.css
-import '@style/App'; // path: src\app\assets\css\App.scss
+import '@style/app'; // path: src\app\assets\css\App.scss
 import '@style/index'; // path: src\app\assets\scss\index.scss
  
 function App(): JSX.Element {
